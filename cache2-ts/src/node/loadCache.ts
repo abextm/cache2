166da4301acdbc2544ec4f9804a06abdaca0f37f
Max Weber <mii7303@gmail.com>
2024-03-16 18:06:50 -0600

    cache2-ts: add node module with node env only helpers

---
+++ cache2-ts/src/node/loadCache.ts
@@ -0,0 +1,29 @@
+import * as fs from "node:fs/promises";
+import * as path from "node:path";
+import { CacheProvider, FileProvider } from "../Cache";
+import { DiskCacheProvider } from "../DiskCache";
+import { FlatCacheProvider } from "../FlatCache";
+
+export class NodeFSFileProvider implements FileProvider {
+	public constructor(private path: string) {
+	}
+
+	public getFile(name: string): Promise<Uint8Array | undefined> {
+		return fs.readFile(path.join(this.path, name));
+	}
+
+	public exists(name: string): Promise<boolean> {
+		return fs.access(path.join(this.path, name)).then(_ => true, _ => false);
+	}
+}
+
+export async function loadCache(filePath: string): Promise<CacheProvider> {
+	let fileProvider = new NodeFSFileProvider(filePath);
+	if (await fileProvider.exists("main_file_cache.dat2")) {
+		return new DiskCacheProvider(fileProvider);
+	}
+	if (await fileProvider.exists("0.flatcache")) {
+		return new FlatCacheProvider(fileProvider);
+	}
+	throw new Error(`Path "${filePath}" does not contain a cache`);
+}
